f759b415c6fb98b112dfa505a81926f969b5aac1
zhaolei <zhl19860301@163.com>
2018-08-29 11:11:17 +0800

    多个节点（Producer端1个、Consumer端2个）

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # myrocketmq
+
+
+一、三种消费 ：1.普通消费 2. 顺序消费 3.事务消费
+
+1.1  顺序消费：在网购的时候，我们需要下单，那么下单需要假如有三个顺序，第一、创建订单 ，第二：订单付款，第三：订单完成。也就是这个三个环节要有顺序，这个订单才有意义。RocketMQ可以保证顺序消费，他的实现是生产者（一个生产者可以对多个主题去发送消息）将这个三个消息放在topic（一个topic默认有4个队列）的一个队列里面，单机支持上万个持久化队列，消费端去消费的时候也是只能有一个Consumer去取得这个队列里面的数据，然后顺序消费。
+
+单个节点（Producer端1个、Consumer端1个）
+略
+多个节点（Producer端1个、Consumer端2个）  
+ProducerMultiple
+Consumer1Multiple
+Consumer2Multiple
+
+https://blog.csdn.net/gwd1154978352/article/details/80802674
+
+https://blog.csdn.net/u010634288/article/details/57158374/
+默认的是 集群消费模式
---
+++ src/main/java/com/example/rocketmq/consumer/Consumer1Multiple.java
@@ -0,0 +1,60 @@
+package com.example.rocketmq.consumer;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerOrderly;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Consumer1Multiple {
+
+    public static void main(String[] args) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("order_Consumer");
+        consumer.setNamesrvAddr("192.168.109.214:9876");
+
+        /**
+         * 设置Consumer第一次启动是从队列头部开始消费还是队列尾部开始消费<br>
+         * 如果非第一次启动，那么按照上次消费的位置继续消费
+         */
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+
+        consumer.subscribe("TopicOrder0829", "*");
+
+        /**
+         * 实现了MessageListenerOrderly表示一个队列只会被一个线程取到
+         *，第二个线程无法访问这个队列
+         */
+        consumer.registerMessageListener(new MessageListenerOrderly() {
+            AtomicLong consumeTimes = new AtomicLong(0);
+
+            @Override
+            public ConsumeOrderlyStatus consumeMessage(List<MessageExt> msgs, ConsumeOrderlyContext context) {
+                // 设置自动提交
+                context.setAutoCommit(true);
+                for (MessageExt msg : msgs) {
+                    System.out.println(msg + ",内容：" + new String(msg.getBody()));
+                }
+
+                try {
+                    TimeUnit.SECONDS.sleep(5L);
+                } catch (InterruptedException e) {
+
+                    e.printStackTrace();
+                }
+                ;
+
+                return ConsumeOrderlyStatus.SUCCESS;
+            }
+        });
+
+        consumer.start();
+
+        System.out.println("Consumer1 Started.");
+    }
+}
